d003509befee069e0915a1aad9e09a64c263f6a6
Your Name <you@example.com>
2019-01-23 11:28:17 +0800

    增加保存路径的注释

--- src/main/scala/com/zhongdakeji/SparkStreamingGetDataFromKafka.scala
+++ src/main/scala/com/zhongdakeji/SparkStreamingGetDataFromKafka.scala
@@ -12,28 +12,29 @@ import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, Loca
   */
 object SparkStreamingGetDataFromKafka {
   val conf = new SparkConf().setAppName("sparkStreamingGetDataFromKafka")
-  val ssc = new StreamingContext(conf,Seconds(3))
-  val kafkaParams = Map("bootstrap.servers"->"bigdata01:9092,bigdata02:9092,bigdata03:9092",
-    "key.deserializer"->"org.apache.kafka.common.serialization.StringDeserializer",
-    "value.deserializer"->"org.apache.kafka.common.serialization.StringDeserializer",
-    "group.id"->"sparkstreamingtest"
+  val ssc = new StreamingContext(conf, Seconds(3))
+  val kafkaParams = Map("bootstrap.servers" -> "bigdata01:9092,bigdata02:9092,bigdata03:9092",
+    "key.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+    "value.deserializer" -> "org.apache.kafka.common.serialization.StringDeserializer",
+    "group.id" -> "sparkstreamingtest"
   )
-  def sparkStreamingGetDataFromKafka()={
-    //需要两个参数：定位策略对象（LocationStrategy）和消费策略对象（ConsumerStrategy）
-    val kafkaStream = KafkaUtils.createDirectStream(ssc,LocationStrategies.PreferConsistent,
-      ConsumerStrategies.Subscribe[String,String](Array("kafkatopic"),kafkaParams))
-    //    kafkaStream.map(record=>(record.key(),record.value(),record.timestamp())).print()
-    kafkaStream.map(x=>x.value()).print()
-    kafkaStream.foreachRDD(item=>{
-      //      item.saveAsTextFile("file:///C:\\Users\\Administrator\\Desktop\\streamingfile\\")
-      item.saveAsTextFile("hdfs://bigdata01:8020/streamingtest/")
-    })
 
+  def sparkStreamingGetDataFromKafka() = {
+    //需要两个参数：定位策略对象（LocationStrategy）和消费策略对象（ConsumerStrategy）
+    val kafkaStream = KafkaUtils.createDirectStream(ssc, LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Subscribe[String, String](Array("kafkatopic"), kafkaParams))
+    //    kafkaStream.map(record=>(record.key(),record.value(),record.timestamp())).print()
+    kafkaStream.map(x => x.value()).print()
+    kafkaStream.foreachRDD(item => {
+      item.saveAsTextFile("file:///C:\\Users\\Administrator\\Desktop\\streamingfile\\")//保存到本地
+      item.saveAsTextFile("hdfs://bigdata01:8020/streamingtest/") //保存到hdfs
+    })
 
 
     ssc.start()
     ssc.awaitTermination()
   }
+
   def main(args: Array[String]): Unit = {
     sparkStreamingGetDataFromKafka()
   }
